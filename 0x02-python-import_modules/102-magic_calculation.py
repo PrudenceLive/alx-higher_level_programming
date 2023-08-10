66dc604d5a72d1d4eb016e467a65e3c44355c71c
PrudenceLive <pmprue328@gmail.com>
2023-08-10 15:45:56 -0700

    task 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for n in range(4, 6):
-            c = add(c, n)
-        return (c)
-
-    else:
-        return(sub(a, b))i
+def magic_calculation(a, b. c)
+    if a < B:
+        return c
