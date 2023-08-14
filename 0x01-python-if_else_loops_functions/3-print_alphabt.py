c77fc51ab5f380d85c87ddd49433b87e6096efdd
PrudenceLive <pmprue328@gmail.com>
2023-08-14 05:47:05 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number:d} Battery street")
+
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
